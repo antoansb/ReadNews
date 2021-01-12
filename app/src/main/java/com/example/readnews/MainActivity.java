702dc7c9878d1da73c577a7862171a6d33300312
Lazar Antonijevic <lakyspider@gmail.com>
2021-01-12 05:55:02 +0100

    Connecting to API/Getting json data

--- app/src/main/java/com/example/readnews/MainActivity.java
+++ app/src/main/java/com/example/readnews/MainActivity.java
@@ -2,10 +2,18 @@ package com.example.readnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,10 +27,89 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DownloadData dlData = new DownloadData();
+        try {
+
+            dlData.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+
+        } catch (Exception e) {
+
+        }
+
         ListView listView = findViewById(R.id.listView);
-
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
-
         listView.setAdapter(arrayAdapter);
     }
+
+    public class DownloadData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnect = null;
+
+            try {
+
+                url = new URL(urls[0]);
+
+                urlConnect = (HttpURLConnection) url.openConnection();
+
+                InputStream inputStream = urlConnect.getInputStream();
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+
+                while (data != -1) {
+
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+
+                JSONArray jsonArray = new JSONArray(result);
+
+                int numberOfNews = 20;
+
+                if (jsonArray.length() < 20) {
+                    numberOfNews = jsonArray.length();
+                }
+
+                for (int i=0; i<numberOfNews; i++) {
+                    String articleId = jsonArray.getString(i);
+                    url = new URL("https://hacker-news.firebaseio.com/v0/item/" + articleId + ".json?print=pretty");
+                    urlConnect = (HttpURLConnection) url.openConnection();
+
+                    inputStream = urlConnect.getInputStream();
+                    inputStreamReader = new InputStreamReader(inputStream);
+
+                    data = inputStreamReader.read();
+
+                    String articleInfo = "";
+
+                    while (data != -1) {
+
+                        char current = (char) data;
+                        articleInfo += current;
+                        data = inputStreamReader.read();
+                    }
+
+                    Log.i("ArticleInfo", articleInfo);
+                }
+
+                Log.i("URL content", result);
+
+                return result;
+
+            }catch (Exception e) {
+
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
 }
