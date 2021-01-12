53c84bf582e946d62019553455a41d1df347c6d7
Lazar Antonijevic <lakyspider@gmail.com>
2021-01-12 05:11:58 +0100

    Starting/layout

--- app/src/main/java/com/example/readnews/MainActivity.java
+++ app/src/main/java/com/example/readnews/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.readnews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> titles = new ArrayList<>();
+
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
